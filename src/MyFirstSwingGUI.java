ddb620d70def4a01466003ab654c334e1fa67a01
DaveDLeague <DaveD@Leagues-iMac.attlocal.net>
2017-10-19 13:35:41 -0700

    update

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -5,26 +9,40 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-		
+
 		//2. Set your JFrame object to be visible
-		
-		//3. Run your program. Do you see your window?
+
+		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-		
+
 		//5. Create and initialize an object of the JPanel class
-		
+
 		//6. Create and initialize an object of the JLabel class
-		
+
 		//7. Set the text of the JLabel to a lovely greeting.
-		
+
 		//8. Add the JPanel object to the JFrame
-		
+
 		//9. Add the JLabel object to the JPanel
-		
+
 		//10. Pack your JFrame.
-		
+
 		//11. Run your program again. Do you see your message.
 		
+		//12. Use the loadImage method to set the icon of the JLabel object.  
+
+		//13. Re-pack the JFrame object.
+		
+		//14. Run the program one more time. Do you see the image?
+	}
+	
+	public static ImageIcon loadImage(){
+		try {
+			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
+		} catch (IOException e) {
+			
+			return null;
+		}
 	}
 }
