d0c499c81318312654b3efe7ac241f11bf46faab
DaveDLeague <DaveD@Leagues-iMac.attlocal.net>
2017-10-19 13:18:07 -0700

    update

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -1,4 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MyFirstSwingGUI {
-
+	public static void main(String[] args) {
+		//1. Create and initialize an object of the JFrame class
+		
+		//2. Set your JFrame object to be visible
+		
+		//3. Run your program. Do you see your window?
+		
+		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
+		
+		//5. Create and initialize an object of the JPanel class
+		
+		//6. Create and initialize an object of the JLabel class
+		
+		//7. Set the text of the JLabel to a lovely greeting.
+		
+		//8. Add the JPanel object to the JFrame
+		
+		//9. Add the JLabel object to the JPanel
+		
+		//10. Pack your JFrame.
+		
+		//11. Run your program again. Do you see your message.
+		
+	}
 }
