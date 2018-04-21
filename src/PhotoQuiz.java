43817eb3c2d5a3e12ee501f9df52829e1b101d41
jaLeague <jackie.arseneau@jointheleague.org>
2018-04-20 17:40:39 -0700

    Update PhotoQuiz.java
    Changed to use JFrame.
    Added default close operation.
    Fixed instructions on getting image address.

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -5,7 +5,6 @@
 
 
 import java.awt.Component;
-import java.awt.Frame;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -18,10 +17,11 @@ import javax.swing.JOptionPane;
 public class PhotoQuiz {
 
 	public static void main(String[] args) throws Exception {
-		Frame quizWindow = new Frame();
+		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
+                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image URL”)
+		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
 
 		// 2. create a variable of type "Component" that will hold your image
 
