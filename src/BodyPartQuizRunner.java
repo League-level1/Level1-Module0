d6422848b5cfdcdba159d427dc62f05dbce098c1
jaLeague <jackie.arseneau@jointheleague.org>
2019-03-05 15:42:48 -0800

    Add files via upload

---
+++ src/BodyPartQuizRunner.java
@@ -0,0 +1,11 @@
+
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+public class BodyPartQuizRunner {
+	public static void main(String[] args) {
+		new BodyPartQuiz().run();
+	}
+}
