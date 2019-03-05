f9cc1f1aba719f8b1f340c439c119e1b60180bdf
jaLeague <jackie.arseneau@jointheleague.org>
2019-03-05 15:43:33 -0800

    Add files via upload
    Added runner classes

---
+++ PhotoQuizRunner.java
@@ -0,0 +1,11 @@
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+
+public class PhotoQuizRunner {
+	public static void main(String[] args) throws Exception {
+		new PhotoQuiz().run();
+	}
+}
