02e59f056b3444ac36a7e07cdd46baa39ad8102b
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2019-10-07 14:34:51 -0700

    update from league central

---
+++ src/_01_houses/HousesRunner.java
@@ -0,0 +1,13 @@
+package _01_houses;
+
+public class HousesRunner {
+
+	public static void main(String[] args) {
+		//Here we call the run() method from the Houses class
+		Houses houseBuilder = new Houses();
+		houseBuilder.run();
+
+
+	}
+
+}
