11fcf7484855f15cc011442a21ff670475ba5b24
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2020-05-03 19:27:46 -0700

    update from league central

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -15,32 +15,32 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public void run() {
 
-		// 1. Create and initialize an object of the JFrame class
+		// 1. Declare and initialize an object of the JFrame class
 
 		// 2. Set your JFrame object to be visible
 
-		// 3. Run your program. Do you see your window? It's probably very
+		// 3. Open MyFirstSwingGUIRunner and run your program. Do you see your window? It's probably very
 		// small.
 
-		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
+		// 4. Use your JFrame to call setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
-		// 5. Create and initialize an object of the JPanel class
+		// 5. Declare and initialize an object of the JPanel class
 
-		// 6. Create and initialize an object of the JLabel class
+		// 6. Declare and initialize an object of the JLabel class
 
-		// 7. Set the text of the JLabel to a lovely greeting.
+		// 7. Set the text of the JLabel to a lovely greeting
 
 		// 8. Add the JPanel object to the JFrame
 
 		// 9. Add the JLabel object to the JPanel
 
-		// 10. Pack your JFrame.
+		// 10. Pack your JFrame
 
-		// 11. Run your program again. Do you see your message.
+		// 11. Run your program again. Do you see your message?
 
-		// 12. Use the loadImage method to set the icon of the JLabel object.
+		// 12. Use your JLabel to call setIcon(loadImage())
 
-		// 13. Re-pack the JFrame object.
+		// 13. Re-pack the JFrame object
 
 		// 14. Run the program one more time. Do you see the image?
 
@@ -50,7 +50,7 @@ public class MyFirstSwingGUI {
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
 		} catch (IOException e) {
-
+			e.printStackTrace()
 			return null;
 		}
 	}
