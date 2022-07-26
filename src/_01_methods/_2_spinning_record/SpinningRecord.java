db5b6e4e7d14957cc0d0e8d9f9e2a2028a2d9362
thorcat <keith.groves@jointheleague.org>
2022-07-25 17:53:38 -0700

    Updated from Central

--- src/_01_methods/_2_spinning_record/SpinningRecord.java
+++ src/_01_methods/_2_spinning_record/SpinningRecord.java
@@ -1,11 +1,6 @@
 package _01_methods._2_spinning_record;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.net.URL;
-
-import javazoom.jl.player.advanced.AdvancedPlayer;
+import game_tools.Sound;
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -44,7 +39,7 @@ public class SpinningRecord extends PApplet {
     static final int WIDTH = 600;
     static final int HEIGHT = 600;
     
-    Song song = new Song("src/awesomeTrack.mp3");
+    Song song = new Song("awesomeTrack.mp3");
     PImage pictureOfRecord;
     
     @Override
@@ -74,83 +69,16 @@ public class SpinningRecord extends PApplet {
         translate(-image.width / 2, -image.height / 2);
     }
 
-    class Song {
-        private int duration;
-        private String songAddress = null;
-        private AdvancedPlayer mp3Player = null;
-        private InputStream songStream = null;
-
+    class Song extends Sound {
         public Song(String songAddress) {
-            this.songAddress = songAddress;
+            super(songAddress);
         }
 
+        @Override
         public void play() {
-            if (songStream == null) {
-                loadFile();
-                if (songStream != null) {
-                    loadPlayer();
-                    start();
-                } else {
-                    System.err.println("ERROR: Unable to load file: " + songAddress);
-                }
-            }
-        }
-
-        public void setDuration(int seconds) {
-            this.duration = seconds;
-        }
-
-        public void stop() {
-            if (mp3Player != null) {
-                mp3Player.close();
-            }
-            songStream = null;
-        }
-
-        private void start() {
-            Thread t = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        if (duration > 0) {
-                            mp3Player.play(duration);
-                        } else {
-                            mp3Player.play();
-                        }
-                    } catch (Exception e) {
-                    }
-                }
-            };
-            t.start();
-        }
-
-        private void loadPlayer() {
-            try {
-                this.mp3Player = new AdvancedPlayer(songStream);
-            } catch (Exception e) {
-            }
-        }
-
-        private void loadFile() {
-            if (songAddress.contains("http"))
-                this.songStream = loadStreamFromInternet();
-            else
-                this.songStream = loadStreamFromComputer();
-        }
-
-        private InputStream loadStreamFromInternet() {
-            try {
-                return new URL(songAddress).openStream();
-            } catch (Exception e) {
-                return null;
-            }
-        }
-
-        private InputStream loadStreamFromComputer() {
-            try {
-                return new FileInputStream(songAddress);
-            } catch (FileNotFoundException e) {
-                return this.getClass().getResourceAsStream(songAddress);
+            if( !super.isPlaying() ) {
+                System.out.println("playing");
+                super.play();
             }
         }
     }
